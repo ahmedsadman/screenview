9c69ad620a296938ea1efae7cf24b96c382f1ed8
KillerQueen-BitesZaDusto <abusadeed@iut-dhaka.edu>
2021-08-07 21:48:07 +0600

    Created navbar and searchbar components

---
+++ client/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+	// <a href='https://www.freepik.com/vectors/pattern'>Pattern vector created by dgim-studio - www.freepik.com</a>
+
+
+
+const Hero = () => {
+	return (
+		<div>
+			<h1>Hello</h1>
+		</div>
+	)
+}
+
+export default Hero

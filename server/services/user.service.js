c8f8005adbe0271e602069bcc4f741708c4ed220
Ahmed Sadman <ahmedsadman.211@gmail.com>
2021-08-08 02:34:16 +0600

    get and create posts

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,10 +1,16 @@
 const User = require('../models/user.model');
+const Post = require('../models/post.model');
 
 class UserService {
   async create(name, email) {
     const user = new User({ name, email });
     await user.save();
     return user;
+  }
+
+  async getUserPosts(userId) {
+    const posts = await Post.find({ author: userId }).lean().exec();
+    return posts;
   }
 }
 

3fee26f4b37229b4e7c7ffc491de6133b502e8b0
Ahmed Sadman <ahmedsadman.211@gmail.com>
2021-08-08 02:36:13 +0600

    rename endpoints to plural for to maintain convention

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,6 @@ const userRouter = require('./routers/user.router');
 const postRouter = require('./routers/post.router');
 
 module.exports = function routes(app) {
-  app.use('/user', userRouter);
-  app.use('/post', postRouter);
+  app.use('/users', userRouter);
+  app.use('/posts', postRouter);
 }

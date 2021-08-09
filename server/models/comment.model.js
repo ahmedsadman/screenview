c96d33111e308ef8d363da205a3a382195bb6135
Ahmed Sadman <ahmedsadman.211@gmail.com>
2021-08-09 19:35:55 +0600

    remove comments

--- server/models/comment.model.js
+++ server/models/comment.model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  post: { // follower
+  post: {
     type: Schema.Types.ObjectId,
     ref: 'Post',
     required: true
   },
-  author: { // followee
+  author: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     required: true

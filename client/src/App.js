400ebed2c3cd68325ead79ad182cf2975d906038
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-11 16:00:13 +0600

    added route protection

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import LandingPage from './pages/LandingPage';
 import SignForm from './components/SignForm';
 import HomePage from './pages/HomePage';
+import ProtectedRoute from './auth/protected-route';
 
 function App() {
   return (
-    <Router>
+    <>
       <Route path="/" exact component={LandingPage} />
-      <Route path="/register" component={SignForm} />
-      <Route path="/feed" component={HomePage} />
+      {/* <Route path="/register" component={SignForm} /> */}
+      <Switch>
+        <ProtectedRoute path="/feed" component={HomePage} />
+      </Switch>
+      
+    </>
     
-    </Router>
   );
 }
 

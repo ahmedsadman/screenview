fde37e2f3f8d777eb2bedcbba241c4a43075b0c2
Ahmed Sadman <ahmedsadman.211@gmail.com>
2021-08-13 23:52:21 +0600

    redirect already logged in users

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,9 +1,12 @@
-import React from 'react'
-import hero from '../assets/1154.jpg'
-import SignIn from '../components/SignIn'
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import hero from '../assets/1154.jpg';
+import SignIn from '../components/SignIn';
 
 const LandingPage = () => {
-	return (
+	const { isAuthenticated } = useAuth0();
+	return isAuthenticated ? <Redirect to='/feed' /> : (
 		<div className="flex flex-col h-16 md:flex-row h-screen items-center">
 			<div className="bg-white hidden lg:block max-w-screen md:w-1/2 xl:w-2/3 justify-center">
 				<img src={hero} alt="hero" className="w-full  max-h-screen object-cover" />

dd95357262c30df6b06ebd5751c72bd7fad63dc6
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-14 17:32:01 +0600

    user status and review post components

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -7,7 +7,7 @@ const SearchBar = ({ keyword }) => {
 			<div className="pt-2 relative mx-auto text-gray-600">
 				<input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none hover:border-gray-600"
 					type="search" name="search" placeholder="Search" />
-				<button type="submit" className="absolute right-0 top-0 mt-4 mr-4 hover:text-gray-900">
+				<button type="submit" className="absolute mt-4 mr-4 top-0 left-52 hover:text-gray-900">
 					<SearchIcon className="h-6 w-6" aria-hidden="true" />
 				</button>
 			</div>

9ed47d13093d9ee8ec35861352395409f56616c9
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-16 00:30:31 +0600

    dynamic searchbar

---
+++ client/src/components/PostSearchBar.js
@@ -0,0 +1,207 @@
+import React, { useState, useEffect, useRef } from 'react'
+import styled from 'styled-components'
+import { IoClose } from 'react-icons/io5'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useClickOutside } from 'react-click-outside-hook'
+import MoonLoader from 'react-spinners/MoonLoader'
+import axios from 'axios'
+import { useDebounce } from '../hooks/debounceHook'
+import MovieShow from './MovieShow'
+import { SearchIcon } from '@heroicons/react/outline'
+
+const SearchBarContainer = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  width: 15rem;
+  background-color: #fff;
+  border-radius: 6px;
+  box-shadow: 0px 2px 12px 3px rgba(0, 0, 0, 0.14);
+`;
+
+
+const CloseIcon = styled(motion.span)`
+  color: #bebebe;
+  font-size: 23px;
+  vertical-align: middle;
+  transition: all 200ms ease-in-out;
+  cursor: pointer;
+  &:hover {
+    color: #dfdfdf;
+  }
+`;
+
+const LineSeperator = styled.span`
+  display: flex;
+  min-width: 100%;
+  min-height: 2px;
+  background-color: #d8d8d878;
+`;
+
+const SearchContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+  overflow-y: auto;
+`;
+
+const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WarningMessage = styled.span`
+  color: #a1a1a1;
+  font-size: 14px;
+  display: flex;
+  align-self: center;
+  justify-self: center;
+`;
+
+const containerVariants = {
+	expanded: {
+	  height: "30em",
+	},
+	collapsed: {
+	  height: "2.7em",
+	},
+}
+
+const containerTransition = { type: "spring", damping: 22, stiffness: 150 }
+
+const PostSearchBar = ({ keyword }) => {
+
+	const [isExpanded, setExpanded] = useState(false)
+	const [parentRef, isClickedOutside] = useClickOutside()
+	const inputRef = useRef()
+	const [searchQuery, setSearchQuery] = useState("")
+	const [isLoading, setLoading] = useState(false)
+	const [tvShows, setTvShows] = useState([])
+	const [noTvShows, setNoTvShows] = useState(false)
+  
+	const isEmpty = !tvShows || tvShows.length === 0
+
+	const changeHandler = (e) => {
+		e.preventDefault()
+		if (e.target.value.trim() === "") setNoTvShows(false)
+	
+		setSearchQuery(e.target.value)
+	  }
+	
+	  const expandContainer = () => {
+		setExpanded(true)
+	  }
+	
+	  const collapseContainer = () => {
+		setExpanded(false)
+		setSearchQuery("")
+		setLoading(false)
+		setNoTvShows(false)
+		setTvShows([])
+		if (inputRef.current) inputRef.current.value = ""
+	  }
+	
+	  useEffect(() => {
+		if (isClickedOutside) collapseContainer()
+	  }, [isClickedOutside])
+	
+	  const prepareSearchQuery = (query) => {
+		const url = `http://api.tvmaze.com/search/shows?q=${query}`
+	
+		return encodeURI(url)
+	  }
+	
+	  const searchTvShow = async () => {
+		if (!searchQuery || searchQuery.trim() === "") return
+	
+		setLoading(true)
+		setNoTvShows(false)
+	
+		const URL = prepareSearchQuery(searchQuery)
+	
+		const response = await axios.get(URL).catch((err) => {
+		  console.log("Error: ", err)
+		})
+	
+		if (response) {
+		  console.log("Response: ", response.data)
+		  if (response.data && response.data.length === 0) setNoTvShows(true)
+	
+		  setTvShows(response.data)
+		}
+	
+		setLoading(false)
+	  }
+	
+	  useDebounce(searchQuery, 500, searchTvShow)
+
+	return (
+		<SearchBarContainer
+			animate={isExpanded ? "expanded" : "collapsed"}
+			variants={containerVariants}
+			transition={containerTransition}
+			ref={parentRef}
+			className="border-2 border-gray-300 h-10"
+		>
+			<div className="relative mx-auto z-0 text-gray-600 flex items-center">
+			
+					<SearchIcon className="p-2 items-center text-center h-10 w-10" aria-hidden="true" />
+				<input className="w-full pr-16 rounded-md text-sm focus:outline-none hover:border-gray-600"
+					placeholder="Search" onFocus={expandContainer} ref={inputRef} value={searchQuery} onChange={changeHandler}/>
+
+				<AnimatePresence>
+					{isExpanded && (
+						<CloseIcon
+							key="close-icon"
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							onClick={collapseContainer}
+							transition={{ duration: 0.2 }}
+						>
+							<IoClose />
+						</CloseIcon>
+					)}
+				</AnimatePresence>
+					
+			</div>
+		
+    
+        
+      
+      {isExpanded && <LineSeperator />}
+      {isExpanded && (
+        <SearchContent>
+          {isLoading && (
+            <LoadingWrapper>
+              <MoonLoader loading color="#000" size={20} />
+            </LoadingWrapper>
+          )}
+          {!isLoading && isEmpty && !noTvShows && (
+            <LoadingWrapper>
+              <WarningMessage>Start typing to Search</WarningMessage>
+            </LoadingWrapper>
+          )}
+          {!isLoading && noTvShows && (
+            <LoadingWrapper>
+              <WarningMessage>No Tv Shows or Series found!</WarningMessage>
+            </LoadingWrapper>
+          )}
+          {!isLoading && !isEmpty && (
+            <>
+              {tvShows.map(({ show }) => (
+                <MovieShow show={show} key={show.id} />
+              ))}
+            </>
+          )}
+        </SearchContent>
+      )}
+    </SearchBarContainer>
+	)
+}
+
+export default PostSearchBar
---
+++ client/src/hooks/debounceHook.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(value, timeout, callback) {
+  const [timer, setTimer] = useState(null);
+
+  const clearTimer = () => {
+    if (timer) clearTimeout(timer);
+  };
+
+  useEffect(() => {
+    clearTimer();
+
+    if (value && callback) {
+      const newTimer = setTimeout(callback, timeout);
+      setTimer(newTimer);
+    }
+  }, [value]);
+}

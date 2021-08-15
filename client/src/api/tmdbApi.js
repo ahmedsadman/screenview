575f7928e90890d30d54a2de0c0e4145fd62232d
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-15 18:29:54 +0600

    Movies are now fetched from api

---
+++ client/src/api/tmdbApi.js
@@ -0,0 +1,9 @@
+const baseUrl = 'https://api.themoviedb.org/3/'
+
+const key = process.env.REACT_APP_TMDB_KEY
+
+export const trendingUrl = () => {return `${baseUrl}trending/movie/day?api_key=${key}`}
+
+export const popularUrl = () => {return `${baseUrl}movie/popular?api_key=${key}`}
+
+export const upcomingUrl = () => {return `${baseUrl}movie/upcoming?api_key=${key}`}

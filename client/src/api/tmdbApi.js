76231ecedf7b5282d7d3c398396644f3fbfeabbb
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-16 01:08:30 +0600

    dynamic search query from tmdb api

--- client/src/api/tmdbApi.js
+++ client/src/api/tmdbApi.js
@@ -7,3 +7,5 @@ export const trendingUrl = () => {return `${baseUrl}trending/movie/day?api_key=$
 export const popularUrl = () => {return `${baseUrl}movie/popular?api_key=${key}`}
 
 export const upcomingUrl = () => {return `${baseUrl}movie/upcoming?api_key=${key}`}
+
+export const searchUrl = () => {return `${baseUrl}search/movie?api_key=${key}&query=`}

c84dfdf966df3eeab1ce43b0f43791a96e71e1e2
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-15 18:29:54 +0600

    Movies are now fetched from api

--- client/src/components/MovieShow.js
+++ client/src/components/MovieShow.js
@@ -3,27 +3,22 @@ import React from 'react'
 
 
 const MovieShow = ({ show }) => {
+	const image_url = `https://image.tmdb.org/t/p/w500${show.poster_path}`
 	return (
 		<div className="flex mt-1 justify-between">
-			
-			
-					<div className="w-full lg:w-1/3 lg:rounded-r max-w-screen max-h-screen object-cover">
-						<img src={show.image} alt={show.title} />
-					</div>
-				
-					<div className="p-2 w-full lg:w-2/3">
-						<h3 className="font-bold text-lg">{show.title}</h3>
-						<h5 className="text-sm mb-4">{show.date}</h5>
-						<div className="grid grid-cols-2 gap-y-4 gap-x-8 flex justify-between">
-							<p>Reviews: {show.reviewCount}</p>
-							<p>Seen By: {show.watchedCount}</p>		
-						</div>
-						{show.reviewText ? <p>{show.reviewText}</p> : ''}
-					</div>
-                
-            
+			<div className="w-full lg:w-1/3 lg:rounded-r max-w-screen max-h-screen object-cover">
+				<img src={image_url} className=""alt={show.title} />
+			</div>
 		
-            
+			<div className="p-2 w-full lg:w-2/3">
+				<h3 className="font-bold text-lg">{show.title}</h3>
+				<h5 className="text-sm mb-4">{show.date}</h5>
+				<div className="grid grid-cols-2 gap-y-4 gap-x-8 flex justify-between">
+					<p>Rating: {show.vote_average}</p>
+					<p>Seen By: {show.watchedCount}</p>		
+				</div>
+				{show.reviewText ? <p>{show.reviewText}</p> : ''}
+			</div>            
         </div>
 	)
 }
--- client/src/components/Recommendation.js
+++ client/src/components/Recommendation.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Tab } from '@headlessui/react'
 import MovieShow from './MovieShow'
 import { Link } from 'react-router-dom'
+import { getRecommendedObject } from '../data/getMovieData'
+import Loading from './Loading'
 
 const getSelectedClassName = (selected) => {
 	const staticClass = 'w-full text-sm leading-5 font-medium rounded-lg bg-white rounded-md p-4 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'
@@ -18,102 +20,30 @@ const getSelectedClassName = (selected) => {
 
 const Recommendation = () => {
 
-	let [categories] = useState({
-		Upcoming: [
-			{
-				id: 1,
-				title: 'Black Widow',
-				date: '5h ago',
-				description: 'Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.',
-				image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2No8qXpkdu4yfaWGtTqh02qo1Dq.jpg',
-				reviewCount: 5,
-				watchedCount: 2,
-				rating: 4,
-			},
-			{
-				id: 2,
-				title: 'Black Widow',
-				date: '5h ago',
-				description: 'Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.',
-				image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2No8qXpkdu4yfaWGtTqh02qo1Dq.jpg',
-				reviewCount: 5,
-				watchedCount: 2,
-				rating: 4,
-			},
-		],
-		Popular: [
-			{
-				id: 1,
-				title: 'Rick & Morty',
-				date: '5h ago',
-				description: 'Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.',
-				image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8kOWDBK6XlPUzckuHDo3wwVRFwt.jpg',
-				reviewCount: 5,
-				watchedCount: 2,
-				rating: 4,
-			},
-			{
-				id: 2,
-				title: 'Black Widow',
-				date: '5h ago',
-				description: 'Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.',
-				image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2No8qXpkdu4yfaWGtTqh02qo1Dq.jpg',
-				reviewCount: 5,
-				watchedCount: 2,
-				rating: 4,
-			},
-		],
-		Trending: [
-			{
-				id: 1,
-				title: 'Black Widow',
-				date: '5h ago',
-				description: 'Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.',
-				image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2No8qXpkdu4yfaWGtTqh02qo1Dq.jpg',
-				reviewCount: 5,
-				watchedCount: 2,
-				rating: 4,
-			},
-			{
-				id: 2,
-				title: 'Black Widow',
-				date: '5h ago',
-				description: 'Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.',
-				image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2No8qXpkdu4yfaWGtTqh02qo1Dq.jpg',
-				reviewCount: 5,
-				watchedCount: 2,
-				rating: 4,
-			},
-			{
-				id: 3,
-				title: 'Black Widow',
-				date: '5h ago',
-				description: 'Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.',
-				image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2No8qXpkdu4yfaWGtTqh02qo1Dq.jpg',
-				reviewCount: 5,
-				watchedCount: 2,
-				rating: 4,
-			},
-			{
-				id: 4,
-				title: 'Black Widow',
-				date: '5h ago',
-				description: 'Supervillains Harley Quinn, Bloodsport, Peacemaker and a collection of nutty cons at Belle Reve prison join the super-secret, super-shady Task Force X as they are dropped off at the remote, enemy-infused island of Corto Maltese.',
-				image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2No8qXpkdu4yfaWGtTqh02qo1Dq.jpg',
-				reviewCount: 5,
-				watchedCount: 2,
-				rating: 4,
-			},
-		],
-	})
+	const [catagories, setCatagories] = useState()
 
+	useEffect(() => {
+		const getRecommended = async () => {
+			const recommendedMovies = await getRecommendedObject()
+			setCatagories(recommendedMovies)
+		}
+		
+		getRecommended()
+		
+		// eslint-disable-next-line
+	}, [])
+
+	console.log(catagories)
+	
+	
 
 	return (
 		<div className="rounded-lg overflow-auto hidden border-2 border-gray-500 xl:block border-opacity-20 max-w-1/4 max-h-3/4">
-			<Tab.Group>
+			{catagories ?
+				<Tab.Group>
 				<div className="bg-white border-b-2 fixed w-1/4 max-w-1/4">
 					<Tab.List className="flex h-16 space-x-1 bg-white rounded-t-lg">
-						{Object.keys(categories).map((category) => (
+						{Object.keys(catagories).map((category) => (
 							<Tab key={category} className={({ selected }) => getSelectedClassName(selected)}>
 								{category}
 							</Tab>
@@ -122,18 +52,18 @@ const Recommendation = () => {
 				</div>
 				<div className="pt-16">
 					<Tab.Panels>
-						{Object.values(categories).map((posts, idx) => (
+						{Object.values(catagories).map((posts, idx) => (
 							<Tab.Panel key={idx}>
 								{posts.map((post) => (
-									<Link to='#' className="bg-white p-1 rounded-md inline-flex justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-									<MovieShow show={post} key={post.id} />
+									<Link to='#' key={post.id} className="bg-white p-1 rounded-md inline-flex justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+										<MovieShow show={post} key={post.id} />
 									</Link>
 								))}
 							</Tab.Panel>
 						))}						
 					</Tab.Panels>
 				</div>
-			</Tab.Group>
+			</Tab.Group> : <Loading/>}
 		</div >
 	)
 }

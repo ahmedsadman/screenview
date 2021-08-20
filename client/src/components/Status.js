e40bcb36ced64e8026012676390ffe6e44627966
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-20 16:13:01 +0600

    fixed post movie link

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -5,6 +5,7 @@ import Comments from './Comments';
 import MovieShow from './MovieShow';
 import API from '../api';
 import { useAuth0 } from '@auth0/auth0-react';
+import MovieShowCard from './MovieShowCard';
 
 
 const Status = ({ post, addComment }) => {
@@ -30,8 +31,8 @@ const Status = ({ post, addComment }) => {
 
 	useEffect(() => {
 		const commentModification = () => {
-			if(post.comments){
-				if(post.comments.length > 2){
+			if (post.comments) {
+				if (post.comments.length > 2) {
 					setVisibleComment(post.comments?.slice(0, 2))
 				} else {
 					setVisibleComment(post?.comments)
@@ -47,12 +48,12 @@ const Status = ({ post, addComment }) => {
 		getMedia();
 		// eslint-disable-next-line
 	}, []);
-	
-	
-	if(post.comments && post.comments.length !== 0){
+
+
+	if (post.comments && post.comments.length !== 0) {
 		commentVisibility = true
 	}
-	
+
 	// TODO: The comment handling logic needs improvement. It should be more simpler
 	const showAllComment = async () => {
 		const token = await getAccessTokenSilently();
@@ -66,60 +67,61 @@ const Status = ({ post, addComment }) => {
 	}
 
 	return (
-	<div>
-		<div className="mt-3 flex flex-col">
-			<div className="bg-white mt-3 border rounded-t-lg shadow-lg">
-				<div className="bg-white w-full shadow-lg border-2 border-gray-100 rounded-lg p-5">
-					<div className="flex items-center">
-						<img className="h-10 w-10 mr-4 rounded-full"
-							src={post?.author?.avatarUrl}
-							alt=""
-						/>
+		<div>
+			<div className="mt-3 flex flex-col">
+				<div className="bg-white mt-3 border rounded-t-lg shadow-lg">
+					<div className="bg-white w-full shadow-lg border-2 border-gray-100 rounded-lg p-5">
+						<div className="flex items-center">
+							<img className="h-10 w-10 mr-4 rounded-full"
+								src={post?.author?.avatarUrl}
+								alt=""
+							/>
+							<div>
+								<h4 className="text-lg text-gray-700">{post.author.name}</h4>
+								<p className="text-sm mt-1 text-gray-400">{post.postDate}</p>
+							</div>
+						</div>
 						<div>
-							<h4 className="text-lg text-gray-700">{post.author.name}</h4>
-							<p className="text-sm mt-1 text-gray-400">{post.postDate}</p>
-						</div>
-					</div>
-					<div>
-						{post.type === 'watch' ? <p className='text-md text-gray-600 mt-2 mb-2'>Is Watching...</p> : 
-							<p className='text-md text-gray-600 mt-2 mb-2'>Posted a Review on</p>}
-						
-						<Link to='#' className="bg-white p-1 rounded-md inline-flex justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-							<MovieShow show={media} key={post.id} />
-						</Link>
-					</div>
-					
-					<div className='flex justify-center'>
-						<div className="bg-white w-full mb-4 mt-2 flex flex-row flex-wrap justify-center">
-							<button className="w-full p-2 hover:bg-gray-100 hover:text-gray-800 flex justify-center rounded-md items-center text-xl text-gray-500 font-semibold" onClick={makeComment}>
-								<ChatIcon className="h-6 w-6" aria-hidden="true" />
-							</button>
-						</div>
-					</div>
+							{post.type === 'watch' ? <p className='text-md text-gray-600 mt-2 mb-2'>Is Watching...</p> :
+								<p className='text-md text-gray-600 mt-2 mb-2'>Posted a Review on</p>}
 
-					{commentArea ? 
-						<div className="rounded-lg bg-gray-50 mb-2 shadow">
-							<form action="#" method="post">
-								<textarea className="bg-gray-100 w-full rounded-t-lg border" rows="3" placeholder="What are your thoughts? " value={commentContent} onChange={(e) => setCommentContent(e.target.value)}></textarea>
-								<div className="text-center pb-2">
-									<button
-										type="submit"
-										onClick={handleCommentSubmit}
-										className="inline-flex justify-center py-2 px-4 shadow-sm text-sm font-medium rounded-md bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-									>
-										Comment
-									</button>
-								</div>
-							</form>
-						</div> : ''}
+							<Link to={`/movie/${media.id}`} className="bg-white p-1 rounded-md inline-flex justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+								<MovieShowCard show={media} />
+							</Link>
 
-					{commentVisibility ? 
-						<div className="w-full">
-							<Comments comments={visibleComment} />
-							<button className="text-xs mt-2 text-gray-400 hover:underline hover:text-gray-500" onClick={showAllComment}>View All Comments</button>
 						</div>
-						: <p className="text-xs mt-2 text-gray-400">Wow Such Empty. Be the first to comment</p>}
-					{/* <div className="bg-white border-4 bg-gray-300 border-white rounded-b-lg shadow p-5 text-xl text-gray-700 content-center font-semibold flex flex-row flex-wrap">
+
+						<div className='flex justify-center'>
+							<div className="bg-white w-full mb-4 mt-2 flex flex-row flex-wrap justify-center">
+								<button className="w-full p-2 hover:bg-gray-100 hover:text-gray-800 flex justify-center rounded-md items-center text-xl text-gray-500 font-semibold" onClick={makeComment}>
+									<ChatIcon className="h-6 w-6" aria-hidden="true" />
+								</button>
+							</div>
+						</div>
+
+						{commentArea ?
+							<div className="rounded-lg bg-gray-50 mb-2 shadow">
+								<form action="#" method="post">
+									<textarea className="bg-gray-100 w-full rounded-t-lg border" rows="3" placeholder="What are your thoughts? " value={commentContent} onChange={(e) => setCommentContent(e.target.value)}></textarea>
+									<div className="text-center pb-2">
+										<button
+											type="submit"
+											onClick={handleCommentSubmit}
+											className="inline-flex justify-center py-2 px-4 shadow-sm text-sm font-medium rounded-md bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+										>
+											Comment
+										</button>
+									</div>
+								</form>
+							</div> : ''}
+
+						{commentVisibility ?
+							<div className="w-full">
+								<Comments comments={visibleComment} />
+								<button className="text-xs mt-2 text-gray-400 hover:underline hover:text-gray-500" onClick={showAllComment}>View All Comments</button>
+							</div>
+							: <p className="text-xs mt-2 text-gray-400">Wow Such Empty. Be the first to comment</p>}
+						{/* <div className="bg-white border-4 bg-gray-300 border-white rounded-b-lg shadow p-5 text-xl text-gray-700 content-center font-semibold flex flex-row flex-wrap">
 						<div className="w-full">
 							<div className="w-full text-left text-xl text-gray-600">
 								@Some Person
@@ -129,10 +131,10 @@ const Status = ({ post, addComment }) => {
 						</div>
 					</div>
 					: <p className="mt-2 text-center text-gray-400 items-center">Wow Such Empty</p> */}
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 	)
 }
 

90ceb044bb5e9870e12ada3d5a0446a5722a68b6
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-21 21:31:46 +0600

    redirection button from user profile to feed

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,6 +4,7 @@ import EditProfile from '../components/EditProfile'
 import Navbar from '../components/Navbar'
 import StaticProfile from '../components/StaticProfile'
 import API from '../api';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
 	const [profileEditStatus, setProfileEditStatus] = useState(false);
@@ -33,13 +34,13 @@ const Profile = () => {
 	useEffect(() => {
 		getUser();
 	}, []);
-	
+
 	return (
 		<div>
 			<div className="fixed max-w-full inset-x-0 z-20">
-      			<Navbar/>
+				<Navbar />
 			</div>
-			
+
 			<div className="md:gap-6 py-24 items-center max-w-full flex flex-col justify-center">
 				<div className="max-w-3/4 w-full mt-16">
 					<div className="md:col-span-1">
@@ -47,24 +48,32 @@ const Profile = () => {
 							<h3 className="text-lg font-medium leading-6 mb-4 text-gray-900">Profile</h3>
 						</div>
 					</div>
-					
-					
+
+
 					<div>
 						<div>
-							{!profileEditStatus ? <StaticProfile user={user} auth0User={_auth0User}/> : <EditProfile auth0User={_auth0User} onProfileSave={onProfileSave} user={user}/>}
+							{!profileEditStatus ? <StaticProfile user={user} auth0User={_auth0User} /> : <EditProfile auth0User={_auth0User} onProfileSave={onProfileSave} user={user} />}
 						</div>
-						{!profileEditStatus?<div className="px-4 mt-2 flex justify-center py-3 bg-gray-50 text-right sm:px-6">
-							 <button
+						{!profileEditStatus ? <div className="px-4 mt-2 flex justify-between py-3 bg-gray-50 text-right sm:px-6">
+							<Link to='/feed'>
+								<button
+									type="submit"
+									className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+								>
+									Back To Feed
+								</button>
+							</Link>
+							<button
 								type="submit"
-								className="inline-flex justify-center w-1/2 block py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-400 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+								className="inline-flex justify-end py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 								onClick={profileEditStatusChanger}
 							>
 								Edit
-							</button> 
-						</div>: ''}
-						
+							</button>
+						</div> : ''}
+
 					</div>
-	
+
 				</div>
 			</div>
 		</div>

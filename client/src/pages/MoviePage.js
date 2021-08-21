4b09e6be09fa907b28e504625539b0c954403812
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-21 15:14:58 +0600

    moviepage with reviews

--- client/src/pages/MoviePage.js
+++ client/src/pages/MoviePage.js
@@ -8,6 +8,7 @@ import ReactPlayer from 'react-player';
 const MoviePage = ({ match }) => {
 	const [movieDetails, setMovieDetails] = useState();
 	const [movieVideos, setMovieVideos] = useState();
+	// const [movieReviews, setMovieReviews] = useState();
 
 	const key = process.env.REACT_APP_TMDB_KEY;
 
@@ -30,6 +31,50 @@ const MoviePage = ({ match }) => {
 	}, [])
 
 	// console.log(movieVideos);
+
+	// Delete later
+	const reviews = [{
+		avatar: 'https://lh3.googleusercontent.com/a-/AOh14GjJNN27t2gdzWaXWwJeG_feC0VeL0u6ahnOPTsi=s96-c',
+		user: 'ur mom',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus ut elit vel ullamcorper. Nam sagittis tincidunt egestas. Curabitur pretium erat et eros suscipit pretium. Sed blandit malesuada nisl in finibus. Vivamus mi quam, mollis vel lorem quis, placerat maximus turpis. Fusce tempus mi ligula, eu fringilla augue laoreet a. Vivamus elementum ex vel ante sollicitudin volutpat. Morbi ultrices tincidunt fringilla.',
+		rating: 7
+	},
+	{
+		avatar: 'https://lh3.googleusercontent.com/a-/AOh14GjJNN27t2gdzWaXWwJeG_feC0VeL0u6ahnOPTsi=s96-c',
+		user: 'ur mom',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus ut elit vel ullamcorper. Nam sagittis tincidunt egestas. Curabitur pretium erat et eros suscipit pretium. Sed blandit malesuada nisl in finibus. Vivamus mi quam, mollis vel lorem quis, placerat maximus turpis. Fusce tempus mi ligula, eu fringilla augue laoreet a. Vivamus elementum ex vel ante sollicitudin volutpat. Morbi ultrices tincidunt fringilla.',
+		rating: 7
+	},
+	{
+		avatar: 'https://lh3.googleusercontent.com/a-/AOh14GjJNN27t2gdzWaXWwJeG_feC0VeL0u6ahnOPTsi=s96-c',
+		user: 'ur mom',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus ut elit vel ullamcorper. Nam sagittis tincidunt egestas. Curabitur pretium erat et eros suscipit pretium. Sed blandit malesuada nisl in finibus. Vivamus mi quam, mollis vel lorem quis, placerat maximus turpis. Fusce tempus mi ligula, eu fringilla augue laoreet a. Vivamus elementum ex vel ante sollicitudin volutpat. Morbi ultrices tincidunt fringilla.',
+		rating: 7
+	},
+	{
+		user: 'ur mom',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus ut elit vel ullamcorper. Nam sagittis tincidunt egestas. Curabitur pretium erat et eros suscipit pretium. Sed blandit malesuada nisl in finibus. Vivamus mi quam, mollis vel lorem quis, placerat maximus turpis. Fusce tempus mi ligula, eu fringilla augue laoreet a. Vivamus elementum ex vel ante sollicitudin volutpat. Morbi ultrices tincidunt fringilla.',
+		rating: 7
+	},
+	{
+		avatar: 'https://lh3.googleusercontent.com/a-/AOh14GjJNN27t2gdzWaXWwJeG_feC0VeL0u6ahnOPTsi=s96-c',
+		user: 'ur mom',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus ut elit vel ullamcorper. Nam sagittis tincidunt egestas. Curabitur pretium erat et eros suscipit pretium. Sed blandit malesuada nisl in finibus. Vivamus mi quam, mollis vel lorem quis, placerat maximus turpis. Fusce tempus mi ligula, eu fringilla augue laoreet a. Vivamus elementum ex vel ante sollicitudin volutpat. Morbi ultrices tincidunt fringilla.',
+		rating: 7
+	},
+	]
+
+	//Movie review ... may change later
+	let movieReview
+	if (reviews && reviews.length > 2) {
+		movieReview = reviews.slice(0, 2);
+	} else {
+		movieReview = reviews
+	}
+
+	const showAllReviews = () => {
+		movieReview = reviews;
+	}
 
 	return (
 		<div>
@@ -69,6 +114,7 @@ const MoviePage = ({ match }) => {
 						</div>
 
 						<div>
+							{/* TODO: create a carasol */}
 							{!movieVideos ? <Loading /> :
 								<div className="lg:rounded-r max-w-screen mx-2 max-h-screen object-cover flex justify-center">
 									{movieVideos.map(video => (
@@ -82,6 +128,39 @@ const MoviePage = ({ match }) => {
 							}
 
 						</div>
+						{/* TODO: Review */}
+
+						{movieReview ?
+							<div className="w-full mt-4">
+								{movieReview.map(review => (
+									<div className="bg-white border-1 bg-gray-100 border-white mb-2 rounded-lg shadow content-center p-2 text-md text-gray-700">
+										<div>
+											<div className="flex items-center justify-start">
+												<img className="h-8 w-8 mr-3 rounded-full"
+													src={review.avatar}
+													alt=""
+												/>
+												<h4 className='text-blue-400 text-weight-600'>@{review.user}</h4>
+											</div>
+											{/* ForLater: change its location */}
+											<div className="flex float-right">
+												<p className='items-center flex justify-end'>{review.rating}<StarIcon className="h-4 w-4 text-yellow-500" /></p>
+											</div>
+
+										</div>
+
+										<div>
+											<p>{review.content}</p>
+
+										</div>
+									</div>
+								))}
+
+								<button className="text-xs mt-2 text-gray-400 hover:underline hover:text-gray-500" onClick={showAllReviews}>View All Reviews ({reviews.length - 1})</button>
+							</div>
+							: <p className="text-xs mt-2 text-gray-400">Wow Such Empty</p>}
+
+
 					</div>
 				</div>
 			}

ea6a9ef9d91b46ff7401a62a0cf075dea06f01a3
KillerQueen <abusadeed@iut-dhaka.edu>
2021-08-22 13:08:35 +0600

    added tooltip to navbar icons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -48,16 +48,20 @@ const Navbar = () => {
 
 
 							<div className="hidden lg:max-w-1/10 md:flex items-center justify-center md:flex-1 lg:w-0">
-								<Link to="/feed" className="ml-8 bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+								<Link to="/feed" className="ml-8 has-tooltip bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+									<span class="tooltip rounded shadow-lg p-2 bg-white-100 text-xs mt-20">Home</span>
 									<HomeIcon className="h-6 w-6" aria-hidden="true" />
 								</Link>
-								<Link to="/connections" className="ml-8 bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+								<Link to="/connections" className="ml-8 has-tooltip bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+									<span class="tooltip rounded shadow-lg p-2 bg-white-100 text-xs mt-20">Connection</span>
 									<UserGroupIcon className="h-6 w-6" aria-hidden="true" />
 								</Link>
-								<Link to="#" className="ml-8 bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+								<Link to="#" className="ml-8 has-tooltip bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+									<span class="tooltip rounded shadow-lg p-2 bg-white-100 text-xs mt-20">Watch List</span>
 									<BookmarkAltIcon className="h-6 w-6" aria-hidden="true" />
 								</Link>
-								<Link to="#" className="ml-8 bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+								<Link to="#" className="ml-8 has-tooltip bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+									<span class="tooltip rounded shadow-lg p-2 bg-white-100 text-xs mt-20">Notification</span>
 									<BellIcon className="h-6 w-6" aria-hidden="true" />
 								</Link>
 							</div>
@@ -106,16 +110,20 @@ const Navbar = () => {
 								</div>
 								<div className="py-6 px-5 space-y-6">
 									<div className="grid grid-cols-2 gap-y-4 gap-x-8 flex justify-between">
-										<Link to="/feed" className="ml-8 bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+										<Link to="/feed" className="ml-8 has-tooltip bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+											<span class="tooltip rounded shadow-lg p-2 bg-white-100 text-xs mt-20">Home</span>
 											<HomeIcon className="h-6 w-6" aria-hidden="true" />
 										</Link>
-										<Link to="/connections" className="ml-8 bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+										<Link to="/connections" className="ml-8 has-tooltip bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+											<span class="tooltip rounded shadow-lg p-2 bg-white-100 text-xs mt-20">Connection</span>
 											<UserGroupIcon className="h-6 w-6" aria-hidden="true" />
 										</Link>
-										<Link to="#" className="ml-8 bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+										<Link to="#" className="ml-8 has-tooltip bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+											<span class="tooltip rounded shadow-lg p-2 bg-white-100 text-xs mt-20">Watch List</span>
 											<BookmarkAltIcon className="h-6 w-6" aria-hidden="true" />
 										</Link>
-										<Link to="#" className="ml-8 bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+										<Link to="#" className="ml-8 has-tooltip bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-500 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+											<span class="tooltip rounded shadow-lg p-2 bg-white-100 text-xs mt-20">Notification</span>
 											<BellIcon className="h-6 w-6" aria-hidden="true" />
 										</Link>
 										<button

04d5b1ab60d27592d0ba6b8f5a82da5136a72494
Sadman Muhib (Samyo) <ahmedsadman.211@gmail.com>
2021-08-27 01:38:11 +0600

    Revert "fix lint issues"
    This reverts commit 4c30af60097f7e2e00df82d2e511f37fb6e06e37.

--- client/src/hooks/useWatchList.js
+++ client/src/hooks/useWatchList.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import API from "../api";
+import { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import API from '../api';
+
 
 const useWatchList = () => {
   const [watchList, setWatchList] = useState([]);
@@ -12,7 +13,7 @@ const useWatchList = () => {
     await api.addToWatchList(title, type, mediaId);
     const res = await api.getUser();
     setWatchList(res.user.watchList);
-  };
+  }
 
   const fetchWatchList = async () => {
     const token = await getAccessTokenSilently();
@@ -20,14 +21,14 @@ const useWatchList = () => {
     const res = await api.getUser();
     const { watchList } = res.user;
     setWatchList(watchList);
-  };
+  }
 
   const removeFromWatchList = async (mediaId) => {
     const token = await getAccessTokenSilently();
     const api = new API(token);
     await api.removeFromWatchList(mediaId);
     fetchWatchList();
-  };
+  }
 
   useEffect(() => {
     fetchWatchList();
--- client/src/pages/WatchListPage.js
+++ client/src/pages/WatchListPage.js
@@ -1,53 +1,54 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
-import PosterCard from "../components/PosterCard";
-import useWatchList from "../hooks/useWatchList";
-import API from "../api";
+import React, { useEffect, useState } from 'react';
+import Navbar from '../components/Navbar';
+import PosterCard from '../components/PosterCard';
+import useWatchList from '../hooks/useWatchList';
+import API from '../api';
 
 const WatchListPage = () => {
-  const { watchList, removeFromWatchList } = useWatchList();
-  const [populatedList, setPopulatedList] = useState([]);
+	const { watchList, removeFromWatchList } = useWatchList();
+	const [populatedList, setPopulatedList] = useState([]);
 
-  const fetchMovieDetails = async () => {
-    const api = new API();
-    const list = [];
-    for (const item of watchList) {
-      const detail = await api.getMediaDetails(item.mediaId);
-      list.push(detail);
-    }
-    setPopulatedList(list);
-  };
+	const fetchMovieDetails = async () => {
+		const api = new API();
+		const list = [];
+		for (let item of watchList) {
+			const detail = await api.getMediaDetails(item.mediaId);
+			list.push(detail);
+		}
+		setPopulatedList(list);
+	};
 
-  const handleWatchListRemove = (mediaId) => {
-    removeFromWatchList(mediaId);
-  };
+	const handleWatchListRemove = (mediaId) => {
+		removeFromWatchList(mediaId);
+	}
 
-  useEffect(() => {
-    fetchMovieDetails();
-  }, [watchList]);
+	useEffect(() => {
+		fetchMovieDetails();
+	}, [watchList]);
 
-  return (
-    <div>
-      <div className="fixed max-w-full inset-x-0 z-20">
-        <Navbar />
-      </div>
+	return (
+		<div>
+			<div className="fixed max-w-full inset-x-0 z-20">
+				<Navbar />
+			</div>
 
-      <div className="py-24 text-center">
-        <h3 className="my-4 text-2xl font-bold">Your Current Watch List</h3>
-        <div className="flex justify-center">
-          <div className="w-1/2 grid grid-cols-4 gap-4 flex justify-between p-4 shadow-lg rounded border-1">
-            {populatedList.map((list) => (
-              <PosterCard
-                show={list}
-                key={list.title}
-                handleWatchListRemove={handleWatchListRemove}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+			<div className="py-24 text-center">
+				<h3 className="my-4 text-2xl font-bold">Your Current Watch List</h3>
+				<div className="flex justify-center">
 
-export default WatchListPage;
+					<div className="w-1/2 grid grid-cols-4 gap-4 flex justify-between p-4 shadow-lg rounded border-1">
+
+						{populatedList.map(list => (
+							<PosterCard show={list} key={list.title} handleWatchListRemove={handleWatchListRemove} />
+						))}
+					</div>
+
+				</div>
+			</div>
+
+
+		</div>
+	)
+}
+
+export default WatchListPage
